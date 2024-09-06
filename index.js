437ae5fe22cab08389ddaaf88af6e209cb4e6b02
antonov196902 <antonov.arsenii@mail.ru>
2024-09-06 12:12:05 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+﻿const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  const name = req.query.name || 'Recruto';
+  const message = req.query.message || 'Давай дружить';
+  
+  res.send(`Hello ${name}! ${message}`);
+});
+
+app.listen(port, () => {
+  console.log(`Сервис запущен на http://localhost:${port}`);
+});
